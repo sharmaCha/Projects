fd6112a3683ebedef10dd821f7978c59974f8c13
sharmaCha <sharma.cha@husky.neu.edu>
2016-08-20 16:12:35 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,40 +1,78 @@
-require(mxnet)
-install.packages("mxnet")
 require(imager)
 require(shiny)
 require(jpeg)
 require(png)
+require(randomForest)
+require(readr)
+#install.packages("pixmap")
+require(pixmap)
+#install.packages("dplyr")
+require(dplyr)
+#install.packages("rgdal")
+require(readbitmap)
+require(rgdal)
+#install.packages("jpeg")  
+require(class)
+require(caTools)
+#install.packages("imager")
+require(imager)
 
-if (!file.exists("Inception/synset.txt")) {
-  download.file("http://webdocs.cs.ualberta.ca/~bx3/data/Inception.zip", destfile =
-                  "Inception.zip")
-  unzip("Inception.zip")
+trainDF <- read.csv("E:\\ADS_Yelp_Analysis\\DataModel\\train_Yelp.csv")
+trainDF <- na.omit(trainDF)
+trainDF <- trainDF[,2:401]
+set.seed(123)
+split <- sample.split(trainDF, SplitRatio = 0.5)
+train <- subset(trainDF, split == TRUE)
+sum(is.na(train))
+
+train <- na.omit(train)
+
+trainDF_temp <- trainDF
+downsampled_matrix = matrix(ncol = 100, nrow = nrow(trainDF_temp))
+for(i in 1:nrow(trainDF_temp)){
+  m = matrix(unlist(trainDF_temp[i, -1]),nrow = 10,byrow = F)
+  img = as.cimg(m)
+  img_resized = resize(img,round(width(img)/2),round(height(img)/2))
+  downsampled_matrix[i, ] = as.vector(img_resized)
 }
 
-model <<- mx.model.load("Inception/Inception_BN", iteration = 39)
+trainDF_downsampled = as.data.frame(downsampled_matrix)
+trainDF_downsampled$labels = trainDF_temp$labels
+trainDF_downsampled = trainDF_downsampled[,c(101, 1:100)]
 
-synsets <<- readLines("Inception/synset.txt")
+trainDF_down <<- trainDF_downsampled
 
-mean.img <<-
-  as.array(mx.nd.load("Inception/mean_224.nd")[["mean_img"]])
+model <<- randomForest(labels ~ ., ntree=500, data=trainDF_down) # default mtry = sqrt(p), ntree = 500
 
-preproc.image <- function(im, mean.image) {
-  # crop the image
-  shape <- dim(im)
-  short.edge <- min(shape[1:2])
-  xx <- floor((shape[1] - short.edge) / 2)
-  yy <- floor((shape[2] - short.edge) / 2) 
-  croped <- crop.borders(im, xx, yy)
-  # resize to 224 x 224, needed by input of the model.
-  resized <- resize(croped, 224, 224)
-  # convert to array (x, y, channel)
-  arr <- as.array(resized) * 255
-  dim(arr) <- c(224, 224, 3)
-  # substract the mean
-  normed <- arr - mean.img
-  # Reshape to format needed by mxnet (width, height, channel, num)
-  dim(normed) <- c(224, 224, 3, 1)
-  return(normed)
+test_images = list(rep(0,1))
+test_mat = matrix(ncol = 401, nrow = 1)
+
+preproc.image <- function(im) {
+  photo_id = strsplit(1, split = ".jpg", fixed=T)
+  photo_id2 = photo_id[[1]]
+  test_mat[1,1] = as.numeric(photo_id2)
+  test_mat[1,1]
+  test_mat  
+  test_images[[1]]= test_true_files
+  img = test_images[[1]]
+  length(dim(img))
+  dim(img)
+  r = as.data.frame(img[ , , 1])
+  g = as.data.frame(img[ , , 2])
+  b = as.data.frame(img[ , , 3])
+  temp = (r + g + b)/3
+  temp = as.vector(temp)
+  temp = c(photo_id2, temp)
+  View(temp)
+  for(j in 2:401){
+    test_mat[1,j] = temp$band1[j]
+    
+  }
+  
+  testDF = as.data.frame(test_mat)
+  testDF = testDF %>% dplyr::rename(photo_id = V1)
+  
+  return(testDF)
 }
 
 shinyServer(function(input, output) {
@@ -73,7 +111,7 @@ shinyServer(function(input, output) {
     },
     title = "Original Image")
   }, deleteFile = FALSE)
- print(input$file1)
+  print(input$file1)
   output$res <- renderText({
     src = if (input$tabs == "Upload Image") {
       if (is.null(input$file1)) {
@@ -98,10 +136,10 @@ shinyServer(function(input, output) {
     }
     
     im <- load.image(src)
-    normed <- preproc.image(im, mean.img)
-    prob <- predict(model, X = normed)
+    normed <- preproc.image(im)
+    prob <- predict(model, newdata=normed, type = "response")
     max.idx <- order(prob[,1], decreasing = TRUE)[1:5]
-    result <- synsets[max.idx]
+    result <- max.idx
     res_str <- ""
     for (i in 1:5) {
       tmp <- strsplit(result[i], " ")[[1]]
@@ -114,3 +152,6 @@ shinyServer(function(input, output) {
   })
   
 })
+
+
+ 
